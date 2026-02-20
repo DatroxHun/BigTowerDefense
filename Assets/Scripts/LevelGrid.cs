eaf8094e99bb3831cd9f860e623c1a3a2c918909
unknown <don140@inf.elte.hu>
2026-02-20 19:33:51 +0100

    Implemented primitive main menu

---
+++ Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelGrid : MonoBehaviour
+{
+    [SerializeField] private int levelCount;
+    [SerializeField] private Gradient backgroundGradient;
+    [SerializeField] private GameObject levelSelector;
+
+    void Start()
+    {
+        for (int i = 1; i <= levelCount; i++)
+        {
+            int localIdx = i;
+            GameObject selector = Instantiate(levelSelector);
+
+            selector.name = $"Level Selector #{i}";
+            selector.transform.SetParent(transform, false);
+
+            Button btn = selector.GetComponent<Button>();
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(() => MainMenu.Instance.LoadScene(localIdx));
+
+            Image img = selector.GetComponent<Image>();
+            img.color = backgroundGradient.Evaluate((Mathf.PI * i) % 1f);
+
+            TextMeshProUGUI text = selector.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = $"{i}";
+            text.color = backgroundGradient.Evaluate((Mathf.PI * i) % 1f) * Color.grey * Color.lightGray;
+        }
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,20 +1,48 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] Image TransitionPanel;
+    public static MainMenu? Instance { get; private set; } = null;
+
+    [SerializeField] private Image TransitionPanel;
+    [SerializeField] private RectTransform LevelSelectorHolder;
+    [SerializeField] private RectTransform ButtonHolder;
+
+    private void Awake()
+    {
+        // Singleton stuff
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
 
     void Start()
     {
         
     }
 
-    // Button Press Callbacks
-
-    public void PlayButtonPressed()
+    public void LoadScene(int sceneIdx)
     {
+        // Check paramter
+        if (sceneIdx == 0)
+        {
+            Debug.LogWarning("Why would you want to load this menu?");
+        }
+        else if (sceneIdx < 0)
+        {
+            Debug.LogError("Negative sceneIdx is not allowed!");
+            return;
+        }
+
         // Transition
         TransitionPanel.gameObject.SetActive(true);
         LeanTween.value(TransitionPanel.gameObject, (float val) =>
@@ -24,6 +52,41 @@ public class MainMenu : MonoBehaviour
             TransitionPanel.color = c;
         }, 0f, 1f, .5f)
         .setEase(LeanTweenType.easeInSine)
-        .setOnComplete(() => SceneManager.LoadSceneAsync(1)); // 1: TestLevel;
+        .setOnComplete(() => SceneManager.LoadSceneAsync(sceneIdx));
+    }
+
+    // Button Press Callbacks
+
+    public void PlayButtonPressed()
+    {
+        ToggleLevelSelector(true);
+    }
+
+    public void OptionButtonPressed()
+    {
+        Debug.LogWarning("Not implemented!");
+    }
+
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    public void BackButtonPressed()
+    {
+        ToggleLevelSelector(false);
+    }
+
+    // Utils
+
+    private void ToggleLevelSelector(bool visible)
+    {
+        LeanTween.value(TransitionPanel.gameObject, (float val) =>
+        {
+            LevelSelectorHolder.anchoredPosition = new Vector2(val, LevelSelectorHolder.anchoredPosition.y);
+            ButtonHolder.anchoredPosition = new Vector2(val + 3000f, ButtonHolder.anchoredPosition.y);
+
+        }, visible ? -3000f : 0f, visible ? 0f : -3000f, .5f)
+        .setEase(LeanTweenType.easeOutSine);
     }
 }
