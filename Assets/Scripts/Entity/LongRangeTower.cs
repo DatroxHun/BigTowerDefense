6cfd04ebd714dc7fffc91172b01dd16e88cb593d
Hujber Ferenc Kristóf <bwsou0@inf.elte.hu>
2026-03-21 21:53:37 +0100

    Added Obstacle layer and Rck prefab. Added simple targeting to LRT; to be expanded with line-of-sight checking.

--- Assets/Scripts/Entity/LongRangeTower.cs
+++ Assets/Scripts/Entity/LongRangeTower.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class LongRangeTower : Tower
 {
@@ -9,6 +11,26 @@ public class LongRangeTower : Tower
 
     protected override void Target()
     {
-        throw new System.NotImplementedException();
+        List<Enemy> enemies = EnemyManager.instance.Enemies;
+
+        
+
+        List<ITarget> targets = enemies
+            .Where(t =>
+                rangeCollider.OverlapPoint(t.transform.position))
+                .Select(e => e as ITarget).ToList();
+
+        this.CurrentTarget = new MultiTarget(targets);
+
+        // for testing; delete later
+        // - K
+        Debug.Log($"[GUNNER] : ELIGABLE TARGETS: {targets.Count}");
+        /*
+        foreach (var target in targets)
+        {
+            Enemy e = target as Enemy;
+            Debug.Log(e.transform.position);
+        }
+         */
     }
 }
