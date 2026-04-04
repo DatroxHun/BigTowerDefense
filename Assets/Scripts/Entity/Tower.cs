9f728a33c11ec2a81172ed88252338f9d8b70e52
Hujber Ferenc Kristóf <bwsou0@inf.elte.hu>
2026-04-04 18:20:30 +0200

    Added lightning bolt

--- Assets/Scripts/Entity/Tower.cs
+++ Assets/Scripts/Entity/Tower.cs
@@ -24,26 +24,6 @@ public abstract class Tower : Entity
         set { rangeCollider.radius = value; }
     }
 
-
-
-    // health, shield, rangeMod, delays
-    public void ModifyBase(StatProvider2 sp)
-    {
-        
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
     protected void Start()
     {
         targeting = StartCoroutine(Targeting());
---
+++ Assets/Scripts/Projectile/LightningBolt.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class LightningBolt : MonoBehaviour
+{
+    [SerializeField]
+    private float flashTime = 0.2f;
+
+    [SerializeField]
+    private int segments = 5;
+
+    [SerializeField]
+    private float perturbance = 0.5f;
+
+    [SerializeField]
+    private float impactRadius = 0.5f;
+
+    private LineRenderer lr;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+    
+    public void Launch(Vector3 start, Vector3 end, Action<Entity> impactEffect, ObjectPool<LightningBolt> pool)
+    {
+        RenderLine(start, end);
+
+        Strike(end, impactEffect);
+
+        StartCoroutine(ReleaseWithDelay(pool));
+    }
+
+    private void Strike(Vector3 target, Action<Entity> impactEffect)
+    {
+        List<Enemy> enemies = EnemyManager.instance.Enemies;
+
+        enemies
+            .Where(t =>
+                Vector3.Distance(target, t.transform.position) < impactRadius)
+            .ToList()
+            .ForEach(enemy => impactEffect?.Invoke(enemy));
+    }
+
+
+    private IEnumerator ReleaseWithDelay(ObjectPool<LightningBolt> pool)
+    {
+        yield return new WaitForSeconds(flashTime);
+        
+        pool.Release(this);
+    }
+
+    public void RenderLine(Vector3 start, Vector3 end)
+    {
+        lr.positionCount = segments + 1;
+
+        for (int i = 0; i <= segments; i++)
+        {
+            float t = (float)i / segments;
+            Vector3 pos = Vector3.Lerp(start, end, t);
+
+            if (0 < i && i < segments)
+            {
+                pos.x += UnityEngine.Random.Range(-perturbance, perturbance);
+                pos.y += UnityEngine.Random.Range(-perturbance, perturbance);
+            }
+            lr.SetPosition(i, pos);
+        }
+    }
+}
