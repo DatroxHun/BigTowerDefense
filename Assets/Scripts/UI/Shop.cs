36f545f2ff3076d8883da4df4d3e953b32de926c
Dániel Losonczi <don140@INF.ELTE.HU>
2026-04-04 14:19:32 +0200

    Added Shop GUI and fixed Enemy behaviour

---
+++ Assets/Scripts/UI/Shop.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shop : MonoBehaviour
+{
+    private bool openShop = false;
+    private bool animatingShop = false;
+
+    private void Start()
+    {
+        Debug.Log($"{transform.localPosition}");
+        Debug.Log($"{transform.position}");
+    }
+
+    // Button Press Callbacks
+    public void CloseButtonPressed()
+    {
+        if (!animatingShop)
+        {
+            animatingShop = true;
+            openShop = !openShop;
+
+            LeanTween.moveLocalX(gameObject, 1920 / 2 + 400 * (openShop ? 0 : 1), .5f)
+                .setEase(LeanTweenType.easeInOutSine)
+                .setOnComplete(() => animatingShop = false);
+        }
+    }
+
+    public void BuyButtonPressed(int id)
+    {
+        Debug.Log($"buy button id: {id}");
+    }
+}
