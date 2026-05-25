608dea11fe6411ad1ca7f8d1a192ae3adb7b9410
Hujber Ferenc Kristóf <bwsou0@inf.elte.hu>
2026-05-25 19:38:03 +0200

    bullet logic replaced with rigidbopdy 2d physics

--- Assets/Scripts/Projectile/Bullet.cs
+++ Assets/Scripts/Projectile/Bullet.cs
@@ -7,7 +7,7 @@ using UnityEngine.Pool;
 public class Bullet : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1.0f;
+    private float speed = 4.0f;
 
     [SerializeField]
     private LayerMask enemyLayer;
@@ -17,10 +17,10 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     private CircleCollider2D impactCollider;
 
-    private Vector3 target;
     private Action<Entity> impactEffect;
 
-    private bool isFlying = false;
+    [SerializeField]
+    private Rigidbody2D bulletBody;
 
     public GameObject Object => gameObject;
 
@@ -28,70 +28,50 @@ public class Bullet : MonoBehaviour
     public void Launch(Vector3 target, Action<Entity> impactEffect, ObjectPool<Bullet> pool)
     {
         this.pool = pool;
-        this.target = target;
         this.impactEffect = impactEffect;
-        isFlying = true;
         ParticlePool.Emit(transform.position, ParticleType.Smoke, sizeMultiplier: 0.3f);
+        Vector3 direction = (target - transform.position).normalized;
+        bulletBody.linearVelocity = direction * speed;
     }
 
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!isFlying)
-            return;
+        int otherLayer = other.gameObject.layer;
 
-        this.transform.position =
-            Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
-        
-        // idea: allow bullet to go further, not just "hit the ground" at the target
-
-        if (Vector3.Distance(transform.position, target) < 0.001f)
+        if ((enemyLayer & (1 << otherLayer)) != 0)
         {
-            Debug.Log($"[BULLET] : HIT TARGET");
-            Arrive();
+
+            Entity hitEntity = other.GetComponent<Entity>();
+            Debug.Log($"[BULLET] : HIT ENEMY");
+            if (hitEntity != null)
+            {
+                impactEffect.Invoke(hitEntity);
+            }
+
+            Cleanup();
         }
-        else if (CheckObstacle())
+        else if ((obstacleLayer & (1 << otherLayer)) != 0)
         {
             Debug.Log($"[BULLET] : HIT OBSTACLE");
-            Arrive();
+            Cleanup();
         }
-
-    }
-
-    // commented section is for enemies to stop the bullets
-    private bool CheckObstacle() =>
-        Physics2D.OverlapPoint(this.transform.position, obstacleLayer);// ||
-        //Physics2D.OverlapPoint(this.transform.position, enemyLayer);
-
-    void Arrive()
-    {
-        isFlying = false;
-        
-        List<Entity> enemies = EnemyManager.instance.Enemies.Select(e => e as Entity).ToList();
-        List<Entity> towers = TowerManager.instance.Towers.Select(t => t as Entity).ToList();
-        List<Entity> entities = enemies.Concat(towers).ToList();
-
-        // if enemies have a collider, then here we need to
-        // check if the impactC and the enemyC overlap !
-        // -K
-        
-        // if not overlap is written here and enemies stop bullets -> breaks damaging!
-        entities
-            .Where(e =>
-                impactCollider.OverlapPoint(e.transform.position))
-            .ToList()
-            .ForEach(hitEntity => impactEffect?.Invoke(hitEntity));
-
-        pool.Release(this);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("GameBounds"))
         {
-            isFlying = false;
             Debug.Log($"[BULLET] : ESCAPED");
-            pool.Release(this);
+            //pool.Release(this);
         }
+    }
+
+    private void Cleanup()
+    {
+        bulletBody.linearVelocity = Vector2.zero;
+        bulletBody.angularVelocity = 0f;
+        pool.Release(this);
     }
 
 }
